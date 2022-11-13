b89e546d6f138dc36e4da1068c62e9965c379ad0
Abhi <abhishesh.mishra.partner@decathlon.com>
2022-11-13 18:39:15 +0530

    login page Ui

---
+++ lib/screens/login.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginDemo(),
+    );
+  }
+}
+
+class LoginDemo extends StatefulWidget {
+  @override
+  _LoginDemoState createState() => _LoginDemoState();
+}
+
+class _LoginDemoState extends State<LoginDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5D0D6),
+     /* appBar: AppBar(
+        title: const Text("Login Page"),
+      ),*/
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 50,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('asset/images/flutter-logo.png')),
+              ),
+            ), Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    height: 100,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Text("WELCOME TO \n E-SCHEDULER",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
+              ),
+            ),
+             Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius:  BorderRadius.circular(10.0),
+                    ),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+             Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+               // obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                      borderRadius:  BorderRadius.circular(10.0),
+                    ),
+
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              margin: const EdgeInsets.all(5.0),
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: () {
+                 /* Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()));*/
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            const SizedBox(
+              height: 0,
+            ),
+            const Text('Don'"t'have an account? Sign up" )
+          ],
+        ),
+      ),
+    );
+  }
+}
